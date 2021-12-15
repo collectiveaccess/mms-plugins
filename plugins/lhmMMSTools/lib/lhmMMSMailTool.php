10d2f632e2b6660dd9e2af5fb9ddfe11b0ff4536
Seth <seth@collectiveaccess.org>
2021-12-15 11:50:28 -0500

    Update header

--- plugins/lhmMMSTools/lib/lhmMMSMailTool.php
+++ plugins/lhmMMSTools/lib/lhmMMSMailTool.php
@@ -1,6 +1,6 @@
 <?php
 /** ---------------------------------------------------------------------
- * app/lib/ca/BaseApplicationTool.php : 
+ * 
  * ----------------------------------------------------------------------
  * CollectiveAccess
  * Open-source collections management software
