184bcc3c8103fc0d9b282203e23258beb9bb38ef
amergelCGI <149600591+amergelCGI@users.noreply.github.com>
2024-05-21 12:14:59 +0200

    Fixed PHP TypeError caused by Zend_Registry::isRegistered()

--- plugins/lhmMMS/lib/mmsHelpers.php
+++ plugins/lhmMMS/lib/mmsHelpers.php
@@ -64,6 +64,8 @@ function mmsGetCurrentUserDisplayName() {
 }
 
 function mmsExtractFloatFromCurrencyValue($ps_value) {
+	global $_locale;
+	
 	$va_matches = array();
 	if(preg_match("!^([\d\.\,]+)([^\d]+)$!u",$ps_value,$va_matches)) {
 		$vs_decimal = $va_matches[1];
@@ -71,11 +73,7 @@ function mmsExtractFloatFromCurrencyValue($ps_value) {
 		$vs_decimal = $va_matches[2];
 	}
 
-	if(Zend_Registry::isRegistered("Zend_Locale")) {
-		$o_locale = Zend_Registry::get('Zend_Locale');
-	} else {
-		$o_locale = new Zend_Locale('de_DE');
-	}
+	$o_locale = $_locale ? $_locale : new Zend_Locale('de_DE');
 	
 	try {
 		return (float) Zend_Locale_Format::getNumber($vs_decimal, array('locale' => $o_locale, 'precision' => 2));
