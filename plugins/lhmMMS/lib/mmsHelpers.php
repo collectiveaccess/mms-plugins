a757710a5c4e8b4793cc1e44f4cf37fd3a1ca101
behnaz.zarriz <behnaz.zarriz@muenchen.de>
2025-09-03 11:00:34 +0200

    Ticket[KULTMMS-2179]: Fix loan insurance recalculation trigger on loan open - Added comparison between stored loan_insurance_value_eur and newly calculated sum - Suppressed automatic warning box on reload when values are identical - Now warning + unsaved changes appear only if the recalculated sum differs - Ensures cleaner UX: no false positives after saving and reloading loans

--- plugins/lhmMMS/lib/mmsHelpers.php
+++ plugins/lhmMMS/lib/mmsHelpers.php
@@ -10,20 +10,28 @@
  * Fügt Warnungs-Box zu aktueller HTTP Antwort hinzu
  * @param string $ps_message Nachricht, die in der Warnungs-Box erscheinen soll
  */
-function mmsAddWarningBox($ps_message) {
+function mmsAddWarningBox($ps_message)
+{
 	global $g_request, $g_response;
 
-	if(is_object($g_response)){
+	if (is_object($g_response)) {
 		/** @var RequestHTTP $g_request */
 		/** @var ResponseHTTP $g_response */
-		if($g_request->getController() != 'ScannerImport') { // keine Warnungs-Boxen beim Scanner import
+		if ($g_request->getController() != 'ScannerImport') { // keine Warnungs-Boxen beim Scanner import
 			$g_response->addContent("
 				<div class='notification-error-box rounded'>
-					<ul class='notification-error-box'>
+				   <ul class='notification-error-box'>
+				   <li class='notification-error-box'>
+				   	<div class='notification-message-container'>
+			       <i class='fa fa-exclamation-triangle' aria-hidden='true' style='font-size: 36px;'></i>
+			       <ul class='notification-error-box'>
 						<li class='notification-error-box'>{$ps_message}</li>
 					</ul>
+				  </div>
+				  </li>
+                   </ul>	
 				</div>
-			",'default');
+			", 'default');
 		}
 	}
 }
@@ -35,9 +43,10 @@ function mmsAddWarningBox($ps_message) {
  * @param string $ps_setting Die Einstellung, die geholt werden soll
  * @return mixed Der Wert für die Einstellung
  */
-function mmsGetSettingFromMMSPluginConfig($ps_setting) {
+function mmsGetSettingFromMMSPluginConfig($ps_setting)
+{
 	// Configuration::load cacht die Objekte, es gibt also keinen Grund, das hier nicht zu machen
-	$o_conf = Configuration::load(__CA_APP_DIR__.'/plugins/lhmMMS/conf/lhmMMS.conf');
+	$o_conf = Configuration::load(__CA_APP_DIR__ . '/plugins/lhmMMS/conf/lhmMMS.conf');
 	return $o_conf->get($ps_setting);
 }
 
@@ -45,48 +54,52 @@ function mmsGetSettingFromMMSPluginConfig($ps_setting) {
  * Holt den aktuell eingeloggten Benutzer. Funktioniert NICHT in CLI Scripten!
  * @return ca_users Der Benutzer
  */
-function mmsGetCurrentUser() {
+function mmsGetCurrentUser()
+{
 	global $g_request;
 
-	if(is_object($g_request)){
+	if (is_object($g_request)) {
 		return $g_request->getUser();
 	} else {
 		return false;
 	}
 }
 
-function mmsGetCurrentUserDisplayName() {
-	if($t_user = mmsGetCurrentUser()) {
+function mmsGetCurrentUserDisplayName()
+{
+	if ($t_user = mmsGetCurrentUser()) {
 		return $t_user->getName();
 	} else {
 		return 'Importer';
 	}
 }
 
-function mmsExtractFloatFromCurrencyValue($ps_value) {
+function mmsExtractFloatFromCurrencyValue($ps_value)
+{
 	global $_locale;
-	
+
 	$va_matches = array();
-	if(preg_match("!^([\d\.\,]+)([^\d]+)$!u",$ps_value,$va_matches)) {
+	if (preg_match('!^([\d\.\,]+)([^\d]+)$!u', $ps_value, $va_matches)) {
 		$vs_decimal = $va_matches[1];
-	} elseif(preg_match("!^([^\d]+)([\d\.\,]+)$!u",$ps_value,$va_matches)) {
+	} elseif (preg_match('!^([^\d]+)([\d\.\,]+)$!u', $ps_value, $va_matches)) {
 		$vs_decimal = $va_matches[2];
 	}
 
 	$o_locale = $_locale ? $_locale : new Zend_Locale('de_DE');
-	
+
 	try {
-		return (float) Zend_Locale_Format::getNumber($vs_decimal, array('locale' => $o_locale, 'precision' => 2));
-	} catch (Zend_Locale_Exception $e){
+		return (float)Zend_Locale_Format::getNumber($vs_decimal, array('locale' => $o_locale, 'precision' => 2));
+	} catch (Zend_Locale_Exception $e) {
 		return 0;
 	}
 }
 
-function mmsFloatToCurrencyValue($pn_value){
+function mmsFloatToCurrencyValue($pn_value)
+{
 	global $_locale;
-	
-	$o_locale = $_locale ? $_locale : new Zend_Locale('de_DE');
- 	$vs_decimal = Zend_Locale_Format::toNumber($pn_value, array('locale' => $o_locale, 'precision' => 2));
 
- 	return $vs_decimal.' EUR';
+	$o_locale = $_locale ? $_locale : new Zend_Locale('de_DE');
+	$vs_decimal = Zend_Locale_Format::toNumber($pn_value, array('locale' => $o_locale, 'precision' => 2));
+
+	return $vs_decimal . ' EUR';
 }
